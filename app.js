cb58ed47f25704fc6713f1dd646c46f0a5ac41a0
ali1ve <aliaga.yahya@gmail.com>
2024-11-28 16:14:12 +0400

    homework

---
+++ app.js
@@ -0,0 +1,29 @@
+let a = 9
+let b = 0
+while(a < 99){
+    a += 2
+    b += a
+}
+console.log(b);
+
+
+
+let number1 = +prompt("Bir ədəd daxil edin:");
+ 
+let number2 = 0
+let i = 99
+
+
+while (i <= 999) {
+    i++;
+    if (i % number1 === 0){
+        number2 += i 
+        console.log(i);
+    }
+    
+}
+console.log(number2);
+
+
+
+
